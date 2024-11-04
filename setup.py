8ba54ea9b7c6383eadbc0df1b09bc102e7beec3d
Shuai <liustark@hotmail.com>
2024-11-04 15:22:06 -0500

    Register Iron one

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ship_env",
+    name="beastadium",
     version="0.0.1",
     install_requires=["gymnasium==0.29.1"],
     packages=find_packages(),
